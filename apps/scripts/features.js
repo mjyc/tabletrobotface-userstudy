dce2c27535952e015209f3eaaaa935379c38bf63
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-20 14:49:58 -0700

    Update features

--- apps/scripts/features.js
+++ apps/scripts/features.js
@@ -1,4 +1,5 @@
 export var defaultFaceFeatures = {
+  stamp: 0,
   isVisible: false,
   faceSize: 0,
   faceCenterX: 0,
@@ -8,72 +9,53 @@ export var defaultFaceFeatures = {
 };
 
 export function extractFaceFeatures(poses) {
-  let isVisible = null;
-  let faceSize = null;
-  let faceCenterX = null;
-  let faceCenterY = null;
-  let faceOrientation = null;
-  let noseOrientation = null;
-
-  isVisible = poses.length === 1;
-  if (!isVisible) {
-    return {  // only isVisible is not null
-      isVisible,
-      faceSize,
-      faceCenterX,
-      faceCenterY,
-      faceOrientation,
-      noseOrientation,
-    };
+  if (poses.length === 0 || (
+    !poses[0].keypoints.find(function (kpt) {return kpt.part === 'nose';})
+    || !poses[0].keypoints.find(function (kpt) {return kpt.part === 'leftEye';})
+    || !poses[0].keypoints.find(function (kpt) {return kpt.part === 'rightEye';})
+  )) {
+    return Object.keys(defaultFaceFeatures).reduce(function(features, name) {
+      if (name === 'isVisible') {
+        features[name] = false;
+      } else {
+        features[name] = defaultFaceFeatures[name];
+      }
+      return features;
+    }, {stamp: Date.now()});
   }
 
-  const person = poses[0];
-  if (
-    !person.keypoints.find(function (kpt) {return kpt.part === 'nose';})
-    || !person.keypoints.find(function (kpt) {return kpt.part === 'leftEye';})
-    || !person.keypoints.find(function (kpt) {return kpt.part === 'rightEye';})
-  ) {
-    return {  // only isVisible is not null
-      isVisible,
-      faceSize,
-      faceCenterX,
-      faceCenterY,
-      faceOrientation,
-      noseOrientation,
-    };
-  }
-  const ns = person.keypoints.filter(function (kpt) {return kpt.part === 'nose';})[0].position;
-  const le = person.keypoints.filter(function (kpt) {return kpt.part === 'leftEye';})[0].position;
-  const re = person.keypoints.filter(function (kpt) {return kpt.part === 'rightEye';})[0].position;
-  const dnsre = Math.sqrt(Math.pow(ns.x - le.x, 2) + Math.pow(ns.y - le.y, 2));
-  const dnsle = Math.sqrt(Math.pow(ns.x - re.x, 2) + Math.pow(ns.y - re.y, 2));
-  const drele = Math.sqrt(Math.pow(re.x - le.x, 2) + Math.pow(re.y - le.y, 2));
-  const s = 0.5 * (dnsre + dnsle + drele);
-  faceSize = Math.sqrt(s * (s - dnsre) * (s - dnsle) * (s - drele));
+  var ns = person.keypoints.filter(function (kpt) {return kpt.part === 'nose';})[0].position;
+  var le = person.keypoints.filter(function (kpt) {return kpt.part === 'leftEye';})[0].position;
+  var re = person.keypoints.filter(function (kpt) {return kpt.part === 'rightEye';})[0].position;
+  var dnsre = Math.sqrt(Math.pow(ns.x - le.x, 2) + Math.pow(ns.y - le.y, 2));
+  var dnsle = Math.sqrt(Math.pow(ns.x - re.x, 2) + Math.pow(ns.y - re.y, 2));
+  var drele = Math.sqrt(Math.pow(re.x - le.x, 2) + Math.pow(re.y - le.y, 2));
+  var s = 0.5 * (dnsre + dnsle + drele);
+  var faceSize = Math.sqrt(s * (s - dnsre) * (s - dnsle) * (s - drele));
+  var faceCenterX = (ns.x + le.x + re.x) / 3;
+  var faceCenterY = (ns.y + le.y + re.y) / 3;
 
-  faceCenterX = (ns.x + le.x + re.x) / 3;
-  faceCenterY = (ns.y + le.y + re.y) / 3;
-
-  const btnEyesPt = {
+  var btnEyesPt = {
     x: (le.x + re.x) * 0.5,
     y: (le.y + re.y) * 0.5,
   };
-  const v = {  // a vector from the point between two eyes to the nose
+  var v = {  // a vector from the point between two eyes to the nose
     x: ns.x - btnEyesPt.x,
     y: ns.y - btnEyesPt.y,
   };
-  faceOrientation = Math.atan2(v.y, v.x);
+  var faceOrientation = Math.atan2(v.y, v.x);
 
-  const dbere = Math.sqrt(Math.pow(btnEyesPt.x - re.x, 2) + Math.pow(btnEyesPt.y - re.y, 2));
-  const dbens = Math.sqrt(Math.pow(ns.x - btnEyesPt.x, 2) + Math.pow(ns.y - btnEyesPt.y, 2));
-  noseOrientation = Math.acos((Math.pow(dbere, 2) + Math.pow(dbens, 2) - Math.pow(dnsre, 2)) / (2 * dbere * dbens));
+  var dbere = Math.sqrt(Math.pow(btnEyesPt.x - re.x, 2) + Math.pow(btnEyesPt.y - re.y, 2));
+  var dbens = Math.sqrt(Math.pow(ns.x - btnEyesPt.x, 2) + Math.pow(ns.y - btnEyesPt.y, 2));
+  var noseOrientation = Math.acos((Math.pow(dbere, 2) + Math.pow(dbens, 2) - Math.pow(dnsre, 2)) / (2 * dbere * dbens));
 
   return {
-    isVisible,
-    faceSize,
-    faceCenterX,
-    faceCenterY,
-    faceOrientation,
-    noseOrientation,
+    stamp: Date.now(),
+    isVisible: isVisible,
+    faceSize: faceSize,
+    faceCenterX: faceCenterX,
+    faceCenterY: faceCenterY,
+    faceOrientation: faceOrientation,
+    noseOrientation: noseOrientation,
   };
 }

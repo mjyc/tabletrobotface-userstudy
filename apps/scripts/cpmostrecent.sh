f1e3c9d5e44c76081dd1446ea9115fcde5a76f86
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-22 11:31:44 -0700

    Refactor cpmostrecent

--- apps/scripts/cpmostrecent.sh
+++ apps/scripts/cpmostrecent.sh
@@ -2,9 +2,6 @@
 
 test -e ./apps/data/fromrobot || mkdirp ./apps/data/fromrobot;
 for ext in mp4 json; do \
-  if [ "${FILENAME}" == "" ]; then \
-    cp "`ls -dtr1 ~/Downloads/Data*${ext} | tail -1`" ./apps/data/fromrobot/;
-  else
-    cp "`ls -dtr1 ~/Downloads/Data*${ext} | tail -1`" ./apps/data/fromrobot/${FILENAME}.${ext};
-  fi
+  [ "$FILENAME" == "" ] && OUT_FILENAME="" || OUT_FILENAME=$FILENAME.$ext
+  cp "`ls -dtr1 ~/Downloads/Data*$ext | tail -1`" ./apps/data/fromrobot/$OUT_FILENAME;
 done;

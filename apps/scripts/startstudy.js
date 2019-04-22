ec9a22d7e4cc33e91fa7746f901b0983e8883c0d
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-22 14:40:13 -0700

    Add startstudy

---
+++ apps/scripts/startstudy.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+
+var now = Date.now();
+var id = `${Math.random().toString(36).substring(2)}-${now}`;
+var filename = `./apps/data/studies/${id}.json`;
+
+fs.writeFileSync(`${filename}`, JSON.stringify({
+  _id: id,
+  createdAt: `${now}`,
+  updatedAt: `${now}`,
+  settings: require('../settings_helper'),
+}, null, 2));
+console.log(filename);

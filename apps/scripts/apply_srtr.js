f935d5ad8ac56d408c77b90d15ba045423692501
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-22 10:07:44 -0700

    WIP: add initial srtr script

---
+++ apps/scripts/apply_srtr.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+console.log('hello!');
+
+// 1. read traces from data/bags/
+// 2. apply srtr
+// 3. output into data/params (write the script in ../package.json)
+
+// const packagePath = process.argv[2];
+// const bumpType = process.argv[3];
+
+// if (!packagePath || packagePath === '') {
+//   console.log("Please provide a relative package.json path as the first argument");
+//   process.exit(1);
+// }

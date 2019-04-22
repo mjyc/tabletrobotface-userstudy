16d3c334751b7255f5c794933e87a39952854def
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-22 11:31:49 -0700

    WIP

--- apps/scripts/apply_srtr.js
+++ apps/scripts/apply_srtr.js
@@ -1,6 +1,30 @@
 #!/usr/bin/env node
 
-console.log('hello!');
+const tracesFile = process.argv[2];  // apps/data/fromrobot/Data*.json
+const correctionsFile = process.argv[3];  // apps/data/fromrobot/Data*.json
+
+const optionsFile = process.argv[3];  // apps/data/params/transitionName/param
+
+if (!packagePath || packagePath === '') {
+  console.log("Please provide a relative package.json path as the first argument");
+  process.exit(1);
+}
+
+const traces = ;
+const corrections = ;
+
+
+const formula = srtr(
+  transAst, parameterMap, traces, corrections, options);
+expect(formula);
+
+
+
+// OUTPUT:
+// * filenames
+// * output getmodel
+
+
 
 // 1. read traces from data/bags/
 // 2. apply srtr

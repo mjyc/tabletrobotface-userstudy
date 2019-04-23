2abf3fc002b01ee5299f16db9cde34a38c2b2df8
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-22 17:55:58 -0700

    Update cpmostrecent

--- apps/scripts/cpmostrecent.sh
+++ apps/scripts/cpmostrecent.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-# test -e ./apps/data/fromrobot || mkdirp ./apps/data/fromrobot;
-# for ext in mp4 json; do \
-#   [ "$FILENAME" == "" ] && OUT_FILENAME="" || OUT_FILENAME=$FILENAME.$ext
-#   cp "`ls -dtr1 ~/Downloads/Data*$ext | tail -1`" ./apps/data/fromrobot/$OUT_FILENAME;
-# done;
-echo "hello!"
+NUM_DATA=`ls -1 ~/Downloads/Data* 2>/dev/null | wc -l`;
+if [ NUM_DATA = 0 ]; then
+  echo "No file matching ~/Downloads/Data* found"
+  exit 1;
+fi
+
+test -e ./apps/data/fromrobot || mkdirp ./apps/data/fromrobot;
+for ext in mp4 json; do \
+  if [ "$FILENAME" == "" ]; then \
+    OUT_FILENAME="`basename ls -dtr1 ~/Downloads/Data*$ext | tail -1`";
+  else
+    OUT_FILENAME="$FILENAME.$ext"
+  fi
+  cp "`ls -dtr1 ~/Downloads/Data*$ext | tail -1`" "./apps/data/fromrobot/$OUT_FILENAME";
+  echo "./apps/data/fromrobot/$OUT_FILENAME"
+done;

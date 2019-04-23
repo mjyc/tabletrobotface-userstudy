a0fd01eb0de2cc0938efd599a6c72e865fed9be5
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-22 17:30:05 -0700

    WIP

--- apps/scripts/cpmostrecent.sh
+++ apps/scripts/cpmostrecent.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-test -e ./apps/data/fromrobot || mkdirp ./apps/data/fromrobot;
-for ext in mp4 json; do \
-  [ "$FILENAME" == "" ] && OUT_FILENAME="" || OUT_FILENAME=$FILENAME.$ext
-  cp "`ls -dtr1 ~/Downloads/Data*$ext | tail -1`" ./apps/data/fromrobot/$OUT_FILENAME;
-done;
+# test -e ./apps/data/fromrobot || mkdirp ./apps/data/fromrobot;
+# for ext in mp4 json; do \
+#   [ "$FILENAME" == "" ] && OUT_FILENAME="" || OUT_FILENAME=$FILENAME.$ext
+#   cp "`ls -dtr1 ~/Downloads/Data*$ext | tail -1`" ./apps/data/fromrobot/$OUT_FILENAME;
+# done;
+echo "hello!"

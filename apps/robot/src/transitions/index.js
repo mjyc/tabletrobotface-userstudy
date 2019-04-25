398eaa3269b91317179b91b50b68d73018647ef2
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-24 17:13:37 -0700

    Add test_transition.js for testing srtr

--- apps/robot/src/transitions/index.js
+++ apps/robot/src/transitions/index.js
@@ -1,2 +1,3 @@
 export {transition as demo} from './demo';
 export {transition as storytelling_PROFESSOR_ARCHIE_MAKES_A_BANG} from './storytelling_PROFESSOR_ARCHIE_MAKES_A_BANG';
+export {transition as test_transition} from './test_transition';
---
+++ apps/robot/src/transitions/test_transition.js
@@ -0,0 +1,64 @@
+// NOTE: might be called twice if transition and emission fncs are called separately
+export function transition(state, inputD, inputC, params) {
+  if (state === 'S0' && inputD.type === 'START') {
+    return {
+      state: 'S1',
+      outputs: {
+        RobotSpeechbubbleAction: 'Tap "Pause" to pause',
+        HumanSpeechbubbleAction: ['Pause'],
+      },
+    };
+  } else if (
+      state === 'S1' && inputD.type === 'HumanSpeechbubbleAction'
+      && inputD.status === 'SUCCEEDED' && inputD.result === 'Pause'
+  ) {
+    return {
+      state: 'S2',
+      outputs: {
+        RobotSpeechbubbleAction: 'Tap "Resume" to resume',
+        HumanSpeechbubbleAction: ['Resume'],
+      },
+    };
+  } else if (
+      state === 'S2' && inputD.type === 'HumanSpeechbubbleAction'
+      && inputD.status === 'SUCCEEDED' && inputD.result === 'Resume'
+  ) {
+    return {
+      state: 'S1',
+      outputs: {
+        RobotSpeechbubbleAction: 'Tap "Pause" to pause',
+        HumanSpeechbubbleAction: ['Pause'],
+      },
+    };
+
+
+  } else if (
+      state === 'S1' && inputD.type === 'Features'
+      && inputC.face.faceCenterX > params.engagedMinX
+  ) {
+    return {
+      state: 'S2',
+      outputs: {
+        RobotSpeechbubbleAction: 'Tap "Resume" to resume',
+        HumanSpeechbubbleAction: ['Resume'],
+      },
+    };
+  } else if (
+      state === 'S2' && inputD.type === 'Features'
+      && inputC.face.faceCenterX < params.engagedMinX
+  ) {
+    return {
+      state: 'S1',
+      outputs: {
+        RobotSpeechbubbleAction: 'Tap "Pause" to pause',
+        HumanSpeechbubbleAction: ['Pause'],
+      },
+    };
+
+  } else {
+    return {
+      state,
+      outputs: null,
+    };
+  }
+};

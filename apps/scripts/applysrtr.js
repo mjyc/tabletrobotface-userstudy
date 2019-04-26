4d9e6da823041f3749c94911af2d258eb7b50298
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-26 06:50:52 -0700

    Add logging

--- apps/scripts/applysrtr.js
+++ apps/scripts/applysrtr.js
@@ -2,6 +2,7 @@
 
 var fs = require('fs');
 var spawn = require('child_process').spawn;
+var log = require('loglevel-debug')('applysrtr');
 var js2smt2 = require('js2smt2');
 var srtr = require('srtr');
 
@@ -16,11 +17,16 @@ if (
   || !parametersFilename
   || !correctionsFilename
 ) {
-  console.log('usage: ./prepannotating {studyID} {dataFilename}');
+  log.log('usage: ./applysrtr {transitionFilename} {parametersFilename} {dataFilename} {correctionsFilename}');
   process.exit(1);
 }
+log.debug('transitionFilename', transitionFilename);
+log.debug('dataFilename', dataFilename);
+log.debug('parametersFilename', parametersFilename);
+log.debug('correctionsFilename', correctionsFilename);
 
 
+// Load data
 var transitionStr = fs.readFileSync(transitionFilename)
   .toString().replace('export ', '');
 var parametersJSON = JSON.parse(fs.readFileSync(parametersFilename));
@@ -29,6 +35,7 @@ var correctionsJSON = JSON.parse(fs.readFileSync(correctionsFilename));
 var settings = require('../settings_helper');
 
 
+// Prepare inputs
 var transAstRaw = js2smt2.parser.parse(transitionStr);
 // remove "outputs" field in ReturnStatements
 var transAst = srtr.astMap(transAstRaw, function (leaf) {
@@ -52,7 +59,7 @@ var transAst = srtr.astMap(transAstRaw, function (leaf) {
 var transAstIfStatement = transAst.body[0].body.body.filter(function(b) {
   return b.type === 'IfStatement';
 })[0];  // must contain at least one IfStatemenet
-// console.log(srtr.astToJS(transAstIfStatement));
+log.debug('transAstIfStatement', srtr.astToJS(transAstIfStatement));
 
 var paramMap = parametersJSON;
 
@@ -66,66 +73,78 @@ var traces = tracesJSON.map(function (t) {
     },
   };
 });
-// console.log(traces);
+log.debug('traces', traces);
 
 var corrections = correctionsJSON.corrections;
+log.debug('corrections', JSON.stringify(corrections));
+corrections.map(function (c) {
+  var trace = traces.filter(function (t) {
+    return t.stamp === c.stamp;
+  })[0];
+  !!trace && log.debug('trace', JSON.stringify(trace), 'correction', JSON.stringify(c));
+})
 
 var options = settings.srtr.options;
 
+
+// Run SRTR
 var z3Input = srtr.createSRTRSMT2(transAstIfStatement, paramMap, traces, corrections, options);
-// console.log(z3Input);
+log.debug('z3Input', z3Input);
 
 var p = spawn('z3', ['-T:5', '-smt2', '-in'], {stdio: ['pipe', 'pipe', 'ignore']});
 p.stdout.on('data', function (data) {
-  // console.log(data.toString());
-  if (!data.toString().startsWith("sat")) {
-    var modelAst = srtr.sexpParser.parse(data.toString());
-    var deltas = modelAst.value.slice(1).reduce(function (acc, v) {
-      if (
-        v.type === 'Expression'
-        && v.value[1].type === 'Atom' && v.value[1].value.type === 'Identifier'
-        && /^delta_./.test(v.value[1].value.name)
-        // && v.value[4].type === 'Atom' && v.value[4].value.type === 'Literal'
-      ) {
-        if (v.value[4].type === 'Atom' && v.value[4].value.type === 'Literal')
-          acc[v.value[1].value.name] = v.value[4].value.value;
-        else {
-          acc[v.value[1].value.name] = -1 * v.value[4].value[1].value.value;
-        }
-      }
-      return acc;
-    }, {});
-    var weights = modelAst.value.slice(1).reduce(function (acc, v) {
-      if (
-        v.type === 'Expression'
-        && v.value[1].type === 'Atom' && v.value[1].value.type === 'Identifier'
-        && /w[0-9]+/.test(v.value[1].value.name)
-        // && v.value[4].type === 'Atom' && v.value[4].value.type === 'Literal'
-      ) {
-        if (v.value[4].type === 'Atom' && v.value[4].value.type === 'Literal')
-          acc[v.value[1].value.name] = v.value[4].value.value;
-        else {
-          acc[v.value[1].value.name] = -1 * v.value[4].value[1].value.value;
-        }
-      }
-      return acc;
-    }, {});
-    // console.log('deltas', deltas);
-    // console.log('weights', weights);
-
-    var prevParams = paramMap;
-    var newParams = Object.keys(prevParams).reduce(function(acc, k) {
-      var kd = 'delta_' + k;
-      acc[k] = prevParams[k] + (!!deltas[kd] ? deltas[kd] : 0);
-      return acc;
-    }, {});
-
-    console.log(options);
-    console.log(deltas);
-    console.log(weights);
-    console.log(prevParams);
-    console.log(newParams);
+  log.debug('z3output', data.toString());
+  if (data.toString().startsWith("sat")) {
+    return;
   }
+  var modelAst = srtr.sexpParser.parse(data.toString());
+  var deltas = modelAst.value.slice(1).reduce(function (acc, v) {
+    if (
+      v.type === 'Expression'
+      && v.value[1].type === 'Atom' && v.value[1].value.type === 'Identifier'
+      && /^delta_./.test(v.value[1].value.name)
+      // && v.value[4].type === 'Atom' && v.value[4].value.type === 'Literal'
+    ) {
+      if (v.value[4].type === 'Atom' && v.value[4].value.type === 'Literal')
+        acc[v.value[1].value.name] = v.value[4].value.value;
+      else {
+        acc[v.value[1].value.name] = -1 * v.value[4].value[1].value.value;
+      }
+    }
+    return acc;
+  }, {});
+  var weights = modelAst.value.slice(1).reduce(function (acc, v) {
+    if (
+      v.type === 'Expression'
+      && v.value[1].type === 'Atom' && v.value[1].value.type === 'Identifier'
+      && /w[0-9]+/.test(v.value[1].value.name)
+      // && v.value[4].type === 'Atom' && v.value[4].value.type === 'Literal'
+    ) {
+      if (v.value[4].type === 'Atom' && v.value[4].value.type === 'Literal')
+        acc[v.value[1].value.name] = v.value[4].value.value;
+      else {
+        acc[v.value[1].value.name] = -1 * v.value[4].value[1].value.value;
+      }
+    }
+    return acc;
+  }, {});
+  log.debug('deltas', deltas);
+  log.debug('weights', weights);
+
+  var inputParams = paramMap;
+  var params = Object.keys(inputParams).reduce(function(acc, k) {
+    var kd = 'delta_' + k;
+    acc[k] = inputParams[k] + (!!deltas[kd] ? deltas[kd] : 0);
+    return acc;
+  }, {});
+
+  var output = {
+    inputParams: inputParams,
+    deltas: deltas,
+    weights: weights,
+    params: params,
+  };
+  console.log(JSON.stringify(output));
 });
 p.stdin.write(z3Input);
 p.stdin.end();

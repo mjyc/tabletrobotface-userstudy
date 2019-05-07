be52e5113c5ab41864e99216141c11ae2af4b2db
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-05-07 15:53:16 -0700

    Update transition

--- apps/robot/src/transitions/test_transition.js
+++ apps/robot/src/transitions/test_transition.js
@@ -6,7 +6,6 @@ function transition(state, inputD, inputC, params) {
   var disengagedMaxNoseAngle = params.disengagedMaxNoseAngle;
   var disengagedTimeoutIntervalMs = params.disengagedTimeoutIntervalMs;
 
-
   if (state === 'S0' && inputD.type === 'START') {
     return {
       state: 'S1',
@@ -39,31 +38,41 @@ function transition(state, inputD, inputC, params) {
     };
 
 
-  } else if (  // disengaged
-      state === 'S1' && inputD.type === 'Features'
-      && (
-        inputC.face.isVisible && (inputC.face.noseAngle > disengagedMaxNoseAngle || inputC.face.noseAngle < disengagedMinNoseAngle)
-        || !inputC.face.isVisible && (inputC.face.stamp - inputC.face.stampLastDetected) > disengagedTimeoutIntervalMs
-      )
-  ) {
-    return {
-      state: 'S2',
-      outputs: {
-        RobotSpeechbubbleAction: 'Tap "Resume" to resume',
-        HumanSpeechbubbleAction: ['Resume'],
-      },
-    };
-  } else if (  // engaged
-      state === 'S2' && inputD.type === 'Features'
-      && inputC.face.isVisible && (inputC.face.noseAngle < engagedMaxNoseAngle && inputC.face.noseAngle > engagedMinNoseAngle)
-  ) {
-    return {
-      state: 'S1',
-      outputs: {
-        RobotSpeechbubbleAction: 'Tap "Pause" to pause',
-        HumanSpeechbubbleAction: ['Pause'],
-      },
-    };
+  } else if (state === 'S1' && inputD.type === 'Features') {
+    if (  // disengaged
+      inputC.face.isVisible && (inputC.face.noseAngle > disengagedMaxNoseAngle || inputC.face.noseAngle < disengagedMinNoseAngle)
+      || !inputC.face.isVisible && (inputC.face.stamp - inputC.face.stampLastDetected) > disengagedTimeoutIntervalMs
+    ) {
+      return {
+        state: 'S2',
+        outputs: {
+          RobotSpeechbubbleAction: 'Tap "Resume" to resume',
+          HumanSpeechbubbleAction: ['Resume'],
+        },
+      };
+    } else {
+      return {
+        state: state,
+        outputs: null,
+      };
+    }
+  } else if (state === 'S2' && inputD.type === 'Features') {
+    if (  // engaged
+      inputC.face.isVisible && (inputC.face.noseAngle < engagedMaxNoseAngle && inputC.face.noseAngle > engagedMinNoseAngle)
+    ) {
+      return {
+        state: 'S1',
+        outputs: {
+          RobotSpeechbubbleAction: 'Tap "Pause" to pause',
+          HumanSpeechbubbleAction: ['Pause'],
+        },
+      };
+    } else {
+      return {
+        state: state,
+        outputs: null,
+      };
+    }
 
 
   } else {
@@ -75,9 +84,11 @@ function transition(state, inputD, inputC, params) {
 };
 
 var defaultParams = {
-  "engagedMinNoseAngle": 80,
-  "engagedMaxNoseAngle": 110,
-  "disengagedTimeoutIntervalMs": 1000
+  "engagedMinNoseAngle": 90,
+  "engagedMaxNoseAngle": 90,
+  "disengagedMinNoseAngle": 0,
+  "disengagedMaxNoseAngle": 180,
+  "disengagedTimeoutIntervalMs": 10000
 };
 
 module.exports = {

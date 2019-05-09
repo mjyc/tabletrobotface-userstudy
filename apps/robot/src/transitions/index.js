d034288971d61bbf84d2e756b49f4c33285708f4
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-05-08 22:19:30 -0700

    BUGFIX

--- apps/robot/src/transitions/index.js
+++ apps/robot/src/transitions/index.js
@@ -1,17 +1,31 @@
-import * as demo from './demo';
-import * as storytelling_professor_archie_makes_a_bang from './storytelling_professor_archie_makes_a_bang';
-import * as test_transition from './test_transition';
+import * as demo from "./demo";
+import * as storytelling_professor_archie_makes_a_bang from "./storytelling_professor_archie_makes_a_bang";
+import * as storytelling_ranger_forester from "./storytelling_ranger_forester";
+import * as test_transition from "./test_transition";
 
-demo.params = require('../../../data/parameters/demo.json');
-storytelling_professor_archie_makes_a_bang.params = require('../../../data/parameters/storytelling_professor_archie_makes_a_bang.json');
-test_transition.params = require('../../../data/parameters/test_transition.json');
+demo.params = require("../../../data/parameters/demo.json");
+storytelling_professor_archie_makes_a_bang.params = require("../../../data/parameters/storytelling_professor_archie_makes_a_bang.json");
+storytelling_ranger_forester.params = require("../../../data/parameters/storytelling_ranger_forester.json");
+test_transition.params = require("../../../data/parameters/test_transition.json");
 
-demo.params = Object.keys(demo.params).length === 0 ? demo.defaultParams : demo.params;
-storytelling_professor_archie_makes_a_bang.params = Object.keys(storytelling_professor_archie_makes_a_bang.params).length === 0 ? storytelling_professor_archie_makes_a_bang.defaultParams : storytelling_professor_archie_makes_a_bang.params;
-test_transition.params = Object.keys(test_transition.params).length === 0 ? test_transition.defaultParams : test_transition.params;
+demo.params =
+  Object.keys(demo.params).length === 0 ? demo.defaultParams : demo.params;
+storytelling_professor_archie_makes_a_bang.params =
+  Object.keys(storytelling_professor_archie_makes_a_bang.params).length === 0
+    ? storytelling_professor_archie_makes_a_bang.defaultParams
+    : storytelling_professor_archie_makes_a_bang.params;
+storytelling_ranger_forester.params =
+  Object.keys(storytelling_ranger_forester.params).length === 0
+    ? storytelling_ranger_forester.defaultParams
+    : storytelling_ranger_forester.params;
+test_transition.params =
+  Object.keys(test_transition.params).length === 0
+    ? test_transition.defaultParams
+    : test_transition.params;
 
 export default {
   demo,
   storytelling_professor_archie_makes_a_bang,
-  test_transition,
+  storytelling_ranger_forester,
+  test_transition
 };

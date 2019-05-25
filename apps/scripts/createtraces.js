c1e750209c07c378460de9d007ff44c4fa508513
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-05-24 19:08:08 -0700

    Add createtraces.js

---
+++ apps/scripts/createtraces.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const xs = require("xstream").default;
+const { mockTimeSource } = require("@cycle/time");
+const log = require("loglevel-debug")("createtraces");
+
+// load file
+const filename = process.argv[2];
+if (!filename) {
+  log.error("usage: ./createtraces {filename}");
+  process.exit(1);
+}
+const data = JSON.parse(fs.readFileSync(filename));
+
+// restore streams
+const Time = mockTimeSource();
+const {schedule, currentTime} = Time.createOperator();
+const sources = Object.keys(data).reduce((prev, label) => ({
+  ...prev,
+  label: xs.create({
+    start: listener => {
+      data[label].map(event => {
+        schedule.next(listener, currentTime() + event.time, event.value);
+      });
+    },
+    stop: () => {}
+  }),
+}), {});
+
+// test
+Object.keys(sources).map(label => sources[label].addListener({
+  next: v => console.log(label, v)
+}));
+Time.run();
